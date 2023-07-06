3a368fc1338f3aaf630a847a101ca3ab207866a7
armiche <serpica.sa@hotmail.com>
2023-07-06 22:47:46 +0100

    primero

--- src/services/authAPIConfig.js
+++ src/services/authAPIConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const authApi = axios.create({
     ///baseURL: 'http://localhost:3001/api'
-    baseURL: 'https://life-server.onrender.com/api',
+    baseURL: 'https://life3server.onrender.com/api',
 })
 
 export default authApi
